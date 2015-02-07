7be9ddc9a4b50b6b23f54ecbdc1994c7f219764f
alex.cosma <alexx.cosma@gmail.com>
2015-02-07 19:38:33 +0200

    test failed

---
+++ test/unit/com/test/unit/Test.groovy
@@ -0,0 +1,14 @@
+package com.test.unit
+
+import grails.test.GrailsUnitTestCase
+import org.apache.tools.ant.taskdefs.optional.extension.Specification
+
+/**
+ * Created by Alex on 07/02/2015.
+ */
+class Test extends GrailsUnitTestCase {
+
+    void "test something"(){
+        assertEquals('mata', 'mama')
+    }
+}
